b1e21dafd2de926be4f49d44ff847585e4a1442b
unknown <diopy92@gmail.com>
2020-05-02 05:50:41 +0200

    premier

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer;
+use App\Entity\Facture;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * L'encodeur de mot de pass
+     * @var UserPasswordEncoderInterface
+     */
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+       $faker = Factory::create('fr_FR');
+
+
+        for ($u=0; $u <mt_rand(5, 20) ; $u++) {
+            $chrono = 1;
+            $user = new User();
+            $hash = $this->encoder->encodePassword($user,"0123456");
+            $user->setPrenom($faker->firstName())
+                ->setNom($faker->lastName())
+                ->setEmail($faker->email())
+                ->setPassword($hash);
+
+                $manager->persist($user);
+
+                for ($c=0; $c < 30; $c++) {
+                    $customer = new Customer();
+                    $customer->setNom($faker->firstName())
+                             ->setPrenom($faker->lastName())
+                             ->setEmail($faker->email())
+                             ->setCompany($faker->company())
+                             ->setUser($user);
+                 $manager->persist($customer);
+                }
+
+                for ($i=0; $i < mt_rand(3 , 10); $i++) {
+                    $facture = new Facture();
+                    $facture->setMontant($faker->randomFloat(2, 250, 5000))
+                            ->setSentAt($faker->dateTimeBetween('-6 months'))
+                            ->setStatus($faker->randomElement(['ENVOYé','PAYé','ANNULé']))
+                            ->setCustomer($customer)
+                            ->setChrono($chrono);
+
+                     $chrono++;
+                     $manager->persist($facture);
+                }
+        }
+
+
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Customer.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
+ */
+class Customer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $company;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Facture", mappedBy="customer")
+     */
+    private $factures;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="customers")
+     */
+    private $user;
+
+    public function __construct()
+    {
+        $this->factures = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Facture[]
+     */
+    public function getFactures(): Collection
+    {
+        return $this->factures;
+    }
+
+    public function addFacture(Facture $facture): self
+    {
+        if (!$this->factures->contains($facture)) {
+            $this->factures[] = $facture;
+            $facture->setCustomer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFacture(Facture $facture): self
+    {
+        if ($this->factures->contains($facture)) {
+            $this->factures->removeElement($facture);
+            // set the owning side to null (unless already changed)
+            if ($facture->getCustomer() === $this) {
+                $facture->setCustomer(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
